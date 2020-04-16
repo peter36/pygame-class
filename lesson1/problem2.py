57205c6c8abbfe7f8637a2c5e2de6f87cc405d97
Peter Li <peter0306@gmail.com>
2020-04-16 14:08:34 -0700

    Homework

--- lesson1/problem2.py
+++ lesson1/problem2.py
@@ -1,1 +1,13 @@
-print("problem2")
+h = 4
+w = 8
+for i in range(0, n):
+    line = ''
+    if (i == 0) or (i == n - 1):
+        for j in range(0, n):
+          line = line + '*'
+    else:
+        line = '*'
+        for j in range(0, n - 2):
+            line = line + ' '
+        line = line + '*'
+    print(line)
