319e61443975b26d1a9afa4b49bbf6b94094a80d
Peter Li <peter0306@gmail.com>
2020-05-05 21:38:45 -0700

    update main.py

--- lesson6/main.py
+++ lesson6/main.py
@@ -8,10 +8,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            print("You pressed {0:c}".format(event.key))
-        elif event.type == pygame.KEYUP:
-            print("You released {0:c}".format(event.key))
 
 pygame.quit()
 
