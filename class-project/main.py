3a8884e6f02fb18c631141a3d300161996deb21f
Peter Li <peter0306@gmail.com>
2020-05-14 20:09:09 -0700

    notes

--- class-project/main.py
+++ class-project/main.py
@@ -1,17 +1,12 @@
-import random
-
 import pygame
 
-# Intialize the pygame
 pygame.init()
 
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-background = pygame.image.load('background.png')
-
 # Caption and Icon
-pygame.display.set_caption("Space Invader")
+pygame.display.set_caption("My Space Invader")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
@@ -21,21 +16,9 @@ playerX = 370
 playerY = 480
 playerX_change = 0
 
-# Enemy
-enemyImg = pygame.image.load('enemy.png')
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 4 # 0.3
-enemyY_change = 40
-
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
-
-
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
-
 
 # Game Loop
 running = True
@@ -44,42 +27,29 @@ while running:
     # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
 
-    # background image
-    screen.blit(background, (0,0))
-
+    # process events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-        # if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -5 # -0.3
+                playerX_change = -0.3
             if event.key == pygame.K_RIGHT:
-                playerX_change = 5 # 0.3
+                playerX_change = 0.3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-    # 5 = 5 + -0.1 -> 5 = 5 - 0.1
-    # 5 = 5 + 0.1
-
-    playerX += playerX_change
-
+    playerX = playerX + playerX_change
     if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
         playerX = 736
-
-    enemyX += enemyX_change
-
-    if enemyX <= 0:
-        enemyX_change = 3 # 0.3
-        enemyY += enemyY_change
-    elif enemyX >= 736:
-        enemyX_change = -3 # -0.3
-        enemyY += enemyY_change
+    elif playerX >= 736:
+        playerX = 0
 
     player(playerX, playerY)
-    enemy(enemyX, enemyY)
+
+    # redraw the screen
     pygame.display.update()
+
+# end of while
+pygame.quit()
